1f69b6b5a567ddfe4dac0334168f64b4bd2528c9
efeceka <efecetinkaya@hotmail.com>
2025-08-11 18:38:40 +0200

    deploy: production build, sitemap & meta updates

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://hastatrasi.com',
+  generateRobotsTxt: false, // robots.txt'i elle koyuyorsan true yapmana gerek yok
+  changefreq: 'weekly',
+  priority: 0.7,
+};
